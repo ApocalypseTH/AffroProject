816a10a5f49f5798db73e8ffc8a5ccaf7a542b52
ApocalypseTH <samuele.sart@yahoo.it>
2018-07-20 16:12:02 +0200

    schermata home inizio scheda utente

---
+++ src/application/HomeController.java
@@ -0,0 +1,49 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class HomeController {
+	
+	static Stage primaryStage;
+	
+	@FXML
+	private MenuItem schedaUtente;
+
+	@FXML
+	private MenuItem ricercheUtenti;
+
+	@FXML
+	private MenuItem ricercheAnalisi;
+
+	@FXML
+	private MenuItem ricercheStorico;
+
+
+	public void schedaU(){
+		SchedaUtente su = new SchedaUtente();
+		try {
+			su.start(primaryStage);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void ricercheU(){
+		
+	}
+	
+	public void ricercheA(){
+		
+	}
+
+	public void ricercheS(){
+	
+	}
+
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -10,8 +12,15 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+
+			FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(Main.class.getResource("home.fxml"));
+	        AnchorPane ap = loader.load();
+	        BorderPane root = new BorderPane();
+	        root.setCenter(ap);
+
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -19,7 +28,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
