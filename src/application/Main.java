e0ac25f144da903d7728c417246ed208292d54d3
ApocalypseTH <samuele.sart@yahoo.it>
2018-07-23 15:22:16 +0200

    agg

--- src/application/Main.java
+++ src/application/Main.java
@@ -24,6 +24,9 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			HomeController.primaryStage = primaryStage;
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
